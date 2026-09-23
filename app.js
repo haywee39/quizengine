bfd97700efca93890b0e4d5de2eaae484074fd71
Your Name <[haywee39@gmail.com]>
2026-09-23 18:18:59 +0100

    Updated the categories and added quesdions for Workers in training

--- app.js
+++ app.js
@@ -10,14 +10,24 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Define categories and questions
     const categories = {
-        "ADULT ENGLISH": {
-            1 : "Question 1 for ADULT ENGLISH: \nQuote Lesson 23, topic and central truth",
-            2: "Question 2 for ADULT ENGLISH: \nQuote the memory verse for Lesson 22.",
-            3: "Question 3 for ADULT ENGLISH: \nQuote Lesson 25, topic and central truth?",
-            4: "Question 4 for ADULT ENGLISH: \nQuote from Lesson 26, \n(a) What circumstances led to the rejection of Saul as the king of Israel? \n(b) What practical lessons can be learnt from the consequences of Saul's disobedience?",
-            5: "Question 5 for ADULT ENGLISH: \nQuote Lesson 29, topic and central truth \n(b) Quote the memory verse for Lesson 29",
-            6: "Question 6 for ADULT ENGLISH: \nQuote Lesson 30, How can we practically incorporate spiritual exercises such as prayer, bible study and worship into our daily routines?",
-            7: "Question 7 for ADULT ENGLISH: \nQuote Lesson 33, How does the fruits of the spirit reinforces the unity of the body of Christ as its members manifest the spiritual gifts?"
+        "VISITORS CLASS": {
+            1 : "Question 1 for VISITORS CLASS: \nQuote Lesson 23, topic and central truth",
+            2: "Question 2 for VISITORS CLASS: \nQuote the memory verse for Lesson 22.",
+            3: "Question 3 for VISITORS CLASS: \nQuote Lesson 25, topic and central truth?",
+            4: "Question 4 for VISITORS CLASS: \nQuote from Lesson 26, \n(a) What circumstances led to the rejection of Saul as the king of Israel? \n(b) What practical lessons can be learnt from the consequences of Saul's disobedience?",
+            5: "Question 5 for VISITORS CLASS: \nQuote Lesson 29, topic and central truth \n(b) Quote the memory verse for Lesson 29",
+            6: "Question 6 for VISITORS CLASS: \nQuote Lesson 30, How can we practically incorporate spiritual exercises such as prayer, bible study and worship into our daily routines?",
+            7: "Question 7 for VISITORS CLASS: \nQuote Lesson 33, How does the fruits of the spirit reinforces the unity of the body of Christ as its members manifest the spiritual gifts?"
+        },
+
+        "BAPTISMAL CLASS": {
+            1 : "Question 1 for BAPTISMAL CLASS: \nQuote Lesson 23, topic and central truth",
+            2: "Question 2 for BAPTISMAL CLASS: \nQuote the memory verse for Lesson 22.",
+            3: "Question 3 for BAPTISMAL CLASS: \nQuote Lesson 25, topic and central truth?",
+            4: "Question 4 for BAPTISMAL CLASS: \nQuote from Lesson 26, \n(a) What circumstances led to the rejection of Saul as the king of Israel? \n(b) What practical lessons can be learnt from the consequences of Saul's disobedience?",
+            5: "Question 5 for BAPTISMAL CLASS: \nQuote Lesson 29, topic and central truth \n(b) Quote the memory verse for Lesson 29",
+            6: "Question 6 for BAPTISMAL CLASS: \nQuote Lesson 30, How can we practically incorporate spiritual exercises such as prayer, bible study and worship into our daily routines?",
+            7: "Question 7 for BAPTISMAL CLASS: \nQuote Lesson 33, How does the fruits of the spirit reinforces the unity of the body of Christ as its members manifest the spiritual gifts?"
         },
 
         "MEMBERSHIP CLASS": {
@@ -28,32 +38,19 @@ document.addEventListener('DOMContentLoaded', () => {
             5: "Question 5 for MEMBERSHIP CLASS: \nWHAT LESSON TREATED THE TOPIC 'HEAVEN'?"
         },
 
-        "BAPTISMAL CLASS": {
-            1: "Question 1 for BAPTISMAL CLASS: \nAccording to the lesson 'The Plan of Salvation'\n_________ is the only way to heaven. \n(a)Water Baptism \n(b)Holy Communion \n(c)Salvation \n(d)Speaking in Tongues",
-            2: "Question 2 for BAPTISMAL CLASS: \nWhat is Salvation?",
-            3: "Question 3 for BAPTISMAL CLASS: \nWhat is the memory verse of the lesson 'Assurance of Salvation'",
-            4: "Question 4 for BAPTISMAL CLASS: \nWhat does Salvation do to us? \nMention any two",
-            5: "Question 5 for BAPTISMAL CLASS: \nWhy do we not baptize babies in Foursquare?",
-            6: "Question 6 for BAPTISMAL CLASS: \nWho planned Salvation?",
-            7: "Question 7 for BAPTISMAL CLASS: \nState 3 importance of Water Baptism",
-            8: "Question 8 for BAPTISMAL CLASS: \nRecite the memory verse of Lesson Two",
-            9: "Question 9 for BAPTISMAL CLASS: \nWhat are the two ordinances Jesus gave his disciples?",
-            10: "Question 10 for BAPTISMAL CLASS: \nWhat is the meaning of  Baptism?"
+        "WORKERS IN TRAINING": {
+            1: "Question 1 for WORKERS IN TRAINING: \n________ is something written down for the purpose of future references, which also serves as an aid to achieve a set goal or objective",
+            2: "Question 2 for WORKERS IN TRAINING: \nDiscipleship is a gradual process.\n True or False ",
+            3: "Question 3 for WORKERS IN TRAINING: \nMention 3 types of discipline.",
+            4: "Question 4 for WORKERS IN TRAINING: \nThe art of a more experienced person guiding the less experienced is called _________",
+            5: "Question 5 for WORKERS IN TRAINING: \nWhat is the topic of Lesson 3?",
+            6: "Question 6 for WORKERS IN TRAINING: \nGod is not very much interested in our spiritual growth. \n True or False",
+            7: "Question 7 for WORKERS IN TRAINING: \nShowing the way with a Christian view point is called _____________ ",
+            8: "Question 8 for WORKERS IN TRAINING: \nMention any 2 biblical examples of mentoring",
+            9: "Question 9 for WORKERS IN TRAINING: \nMention the 3 factors that influences Personality Development according to Lesson 3",
+            10: "Question 10 for WORKERS IN TRAINING: \nWhat is Discipline?"
         },
 
-        "TEENS MEMEBERSHIP": {
-            1: "Question 1 for TEENS MEMEBERSHIP:	\n_______________ and ____________ are the two ordinances that the Foursquare Gospel Church believe in. \nA. Baptism & Reincarnation \n B. The Lord’s Supper & Water Baptism \n C. Holy Baptism & The Lord’s Supper",
-            2: "Question 2 for TEENS MEMEBERSHIP:	\nThe two types of church are? \nA. Visible and Invisible church \nB. Heavenly and Earthly Church \nC. Foursquare Gospel Church and Others ",
-            3: "Question 3 for TEENS MEMEBERSHIP:	\nThe key to daily walking worthy of the Lord unto all pleasing is \nA. Fasting & Prayer \nB. The Secret Place \nC. Meditation",
-            4: "Question 4 for TEENS MEMEBERSHIP:	\nVocal Gifts, Fire Gifts and Revelation Gifts are categories of the gifts of the Holy Spirit according to 1 Cor. 12:1-11. \nTrue \nFalse",
-            5: "Question 5 for TEENS MEMEBERSHIP:	\nThe nine gifts of the Holy Spirit mentioned in 1 Cor. 12:1-11 are the only gifts available to the Church. \nTrue \nFalse",
-            6: "Question 6 for TEENS MEMEBERSHIP:	\nRighteousness and holiness are the same. True \nfalse",
-            7: "Question 7 for TEENS MEMEBERSHIP:	\nState lesson 14 and recite the memory verse for the lesson",
-            8: "Question 8 for TEENS MEMEBERSHIP:	\nState lesson 5 and recite the memory verse of the lesson.",
-            9: "Question 9 for TEENS MEMBERSHIP:	\nMention the nine (9) fruit of the Spirit according to Gal. 5:22-23",
-            10: "Question 10 for TEENS MEMEBERSHIP:	\nState lesson one and quote the memory verse for the lesson."
-        },
-        
         "ADULT YORUBA": {
             1: "Question 1 for ADULT YORUBA: \nEko wo ni ati ri 'Idapo pelu Kristi'?",
             2: "Question 2 for ADULT YORUBA: \nDaruko ayoka kan tio wa fun idapo pelu Kristi",
@@ -62,18 +59,40 @@ document.addEventListener('DOMContentLoaded', () => {
             5: "Question 5 for ADULT YORUBA: \nKiki Oba Kaabo, kini ese lati ranti? \nEko wo lowa?"
         },
 
-        "TEENS CLASS": {
-            1: "Question 1 for TEENS CLASS: \nLesson 34: God's Nation Delivered through Moses \nList the lesson outline under this lesson",
-            2: "Question 2 for TEENS CLASS: \nThe children of Israel dwelt in Egypt for how many years? ",
-            3: "Question 3 for TEENS CLASS: \nLesson 35: The Ten Commandments. \nRecite the memory verse of this lesson",
-            4: "Question 4 for TEENS CLASS: \nList the ten commandments God gave to the children of Israel.",
-            5: "Question 5 for TEENS CLASS: \nWhat is the essence of the commandments?",
-            6: "Question 6 for TEENS CLASS: \nWhat are the  consequences of sin?",
-            7: "Question 7 for TEENS CLASS: \nLesson 36: Creating a New People (from the wilderness) \nList the lesson outline. \nExplain the first two outline. \nMention the name of people that came with positive feedback ",
-            8: "Question 8 for TEENS CLASS: \nLesson 32: Concept of Sin and Righteousness. \nMention the lesson outline under this lesson",
-            9: "Question 9 for TEENS CLASS: \nThe acronym of SIN is what? \nRecite Romans 6:23 ?",
-            10: "Question 10 for TEENS CLASS: \nList the 10 plagues that God caused upon the children of Egypt in order to deliver them from Pharaoh"
+        "ADULT ENGLISH": {
+            1: "Question 1 for ADULT ENGLISH: \nEko wo ni ati ri 'Idapo pelu Kristi'?",
+            2: "Question 2 for ADULT ENGLISH: \nDaruko ayoka kan tio wa fun idapo pelu Kristi",
+            3: "Question 3 for ADULT ENGLISH: \nKini akosori ti owa ni Eko 4?",
+            4: "Question 4 for ADULT ENGLISH: \nOlorun Metalokan Ayeraye, ni'bo  ni ari ayoka re wa?",
+            5: "Question 5 for ADULT ENGLISH: \nKiki Oba Kaabo, kini ese lati ranti? \nEko wo lowa?"
+        },
+
+        "CHILDREN DEPT": {
+            1: "Question 1 for CHILDREN DEPT: \nAccording to the lesson 'The Plan of Salvation'\n_________ is the only way to heaven. \n(a)Water Baptism \n(b)Holy Communion \n(c)Salvation \n(d)Speaking in Tongues",
+            2: "Question 2 for CHILDREN DEPT: \nWhat is Salvation?",
+            3: "Question 3 for CHILDREN DEPT: \nWhat is the memory verse of the lesson 'Assurance of Salvation'",
+            4: "Question 4 for CHILDREN DEPT: \nWhat does Salvation do to us? \nMention any two",
+            5: "Question 5 for CHILDREN DEPT: \nWhy do we not baptize babies in Foursquare?",
+            6: "Question 6 for CHILDREN DEPT: \nWho planned Salvation?",
+            7: "Question 7 for CHILDREN DEPT: \nState 3 importance of Water Baptism",
+            8: "Question 8 for CHILDREN DEPT: \nRecite the memory verse of Lesson Two",
+            9: "Question 9 for CHILDREN DEPT: \nWhat are the two ordinances Jesus gave his disciples?",
+            10: "Question 10 for CHILDREN DEPT: \nWhat is the meaning of  Baptism?"
+        },
+
+        "SEMI-TEENS DEPT": {
+            1: "Question 1 for SEMI-TEENS DEPT:	\n_______________ and ____________ are the two ordinances that the Foursquare Gospel Church believe in. \nA. Baptism & Reincarnation \n B. The Lord’s Supper & Water Baptism \n C. Holy Baptism & The Lord’s Supper",
+            2: "Question 2 for SEMI-TEENS DEPT:	\nThe two types of church are? \nA. Visible and Invisible church \nB. Heavenly and Earthly Church \nC. Foursquare Gospel Church and Others ",
+            3: "Question 3 for SEMI-TEENS DEPT:	\nThe key to daily walking worthy of the Lord unto all pleasing is \nA. Fasting & Prayer \nB. The Secret Place \nC. Meditation",
+            4: "Question 4 for SEMI-TEENS DEPT:	\nVocal Gifts, Fire Gifts and Revelation Gifts are categories of the gifts of the Holy Spirit according to 1 Cor. 12:1-11. \nTrue \nFalse",
+            5: "Question 5 for SEMI-TEENS DEPT:	\nThe nine gifts of the Holy Spirit mentioned in 1 Cor. 12:1-11 are the only gifts available to the Church. \nTrue \nFalse",
+            6: "Question 6 for SEMI-TEENS DEPT:	\nRighteousness and holiness are the same. True \nfalse",
+            7: "Question 7 for SEMI-TEENS DEPT:	\nState lesson 14 and recite the memory verse for the lesson",
+            8: "Question 8 for SEMI-TEENS DEPT:	\nState lesson 5 and recite the memory verse of the lesson.",
+            9: "Question 9 for SEMI-TEENS DEPT:	\nMention the nine (9) fruit of the Spirit according to Gal. 5:22-23",
+            10: "Question 10 for SEMI-TEENS DEPT:	\nState lesson one and quote the memory verse for the lesson."
         }
+        
     };
 
     // Add a default prompt to the category dropdown
